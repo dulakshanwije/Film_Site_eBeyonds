a225c14280a8b21ec5461894685fcd0c8e625d54
Dulakshan Wijesundara <dulakshanwije@gmail.com>
2024-05-10 13:09:23 +0530

    Added Side Menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,8 @@ function removeMovieCard(ele) {
   const card = ele.parentElement.parentElement;
   card.style.display = "none";
 }
+
+function toggleSideMenu() {
+  const menu = document.getElementById("sidemenu");
+  menu.classList.toggle("sidemenu--open");
+}
